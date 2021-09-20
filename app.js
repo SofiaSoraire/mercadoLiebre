0ce9639edec274af11292c993a5db7d8bd02334c
SofiaSoraire <sofiasoraire13@gmail.com>
2021-09-20 18:34:43 -0300

    primerCommit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express= require('express');
+const app=express();
+
+const path=require('path');
+const publicPath= path.join(__dirname, './public')
+
+app.use(express.static(publicPath));
+
+const PORT=process.env.PORT || 3000;
+
+// app.listen(port, ()=> console.log("server up at: http://localhost:3000/"));
+app.listen(PORT, () => {
+    console.log('App listening on http://localhost:3000');
+});
+app.get('/home',(req,res) => {res.sendFile(path.join(__dirname, '/views/home.html'))});
+
+app.get('/login',(req,res) => {res.sendFile(path.join(__dirname, '/views/login.html'))});
+
+app.get('/register',(req,res) => {res.sendFile(path.join(__dirname, '/views/register.html'))});
